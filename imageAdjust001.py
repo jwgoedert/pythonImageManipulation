d00421bc08b1e3a5043c575235b7afb312ce0dea
James Wesley Goedert <jwgoedert@gmail.com>
2022-10-08 04:14:29 -0500

    Add brightness, darkness and grayscale adjustments

--- imageAdjust001.py
+++ imageAdjust001.py
@@ -6,3 +6,16 @@ img_resized = img.resize(newsize)
 print(img.size)
 print(img_resized.size)
 img_resized.save("resized.jpg")
+
+# Define enhancer
+enhancer = ImageEnhance.Brightness(img)
+img_light = enhancer.enhance(1.8)
+img_light.save("brightened.jpg")
+
+enhancer = ImageEnhance.Brightness(img)
+img_light = enhancer.enhance(0.8)
+img_light.save("darkened.jpg")
+
+# img = Image.open('testme.jpg')
+img = img.convert('L')
+img.save('grayscaled.jpg')
